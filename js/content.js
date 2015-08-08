a3456d5d98ce362dab94f3a43f23f24649b9acc9
Adel Qalieh <aqalieh95@gmail.com>
2015-08-08 03:03:18 -0400

    Add bayesian estimate and add documentation

--- js/content.js
+++ js/content.js
@@ -1,3 +1,8 @@
+/**
+ * Extract the number of ratings
+ * @method numberRatings
+ * @return {number} number of ratings
+ */
 function numberRatings() {
   var reviewsRegex = /\d+/;
   var numberReviewsNode = document.getElementById("acrCustomerReviewText");
@@ -5,6 +10,11 @@ function numberRatings() {
   return parseInt(numberReviewsString[0]);
 }
 
+/**
+ * Extract percentages for each star rating
+ * @method starRatingPercents
+ * @return array of rankings
+ */
 function starRatingPercents() {
   var ratingNodes = jQuery('tr.a-histogram-row span.a-size-small:not(:has(*))');
   var ratings = [];
@@ -14,4 +24,17 @@ function starRatingPercents() {
     ratings.push(parseInt(ratingPercent) * 0.01);
   });
   return ratings;
+}
+
+/**
+ * Calculate a ranking using a Bayesian estimate
+ * @method bayesianEstimate
+ * @param {number} R average rating
+ * @param {number} v number of votes
+ * @param {number} m minimum number of votes
+ * @param {number} C the mean vote across all of Amazon
+ * @return weighted ranking
+ */
+function bayesianEstimate(R, v, m, C) {
+  return (v / (v + m)) * R + (m / (v + m)) * C;
 }
